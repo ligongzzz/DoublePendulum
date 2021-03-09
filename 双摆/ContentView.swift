4cada1ebfbb80894a5ed6d0fd3f4918d4fd45a69
xz2000 <xiezhe20001128@sjtu.edu.cn>
2021-03-09 08:06:28 +0800

    Initial Commit

---
+++ 双摆/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  双摆
+//
+//  Created by 谢哲 on 2021/3/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 双摆/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  双摆
+//
+//  Created by 谢哲 on 2021/3/9.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
