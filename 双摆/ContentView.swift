0797b999e9cdcb2d81e8712798d72645749aed42
xz2000 <xiezhe20001128@sjtu.edu.cn>
2021-03-09 19:43:11 +0800

    Show FPS and control the accuracy.

--- 双摆/ContentView.swift
+++ 双摆/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
     @State var active = false
     @StateObject var tmpSolver = SwiftSolver()
     @State var zoom = 100.0
+    @State var fps = 0.0
+    @State var accuracy = 4.5
     
     var body: some View {
         NavigationView{
@@ -35,7 +37,7 @@ struct ContentView: View {
                 }.disabled(active)
                 
                 Section(header: Text("模拟器")) {
-                    NavigationLink(destination: SwiftUIView(swiftSolver: tmpSolver, active: $active, zoom: $zoom), isActive: $active){
+                    NavigationLink(destination: SwiftUIView(swiftSolver: tmpSolver, active: $active, zoom: $zoom, fps: $fps, accuracy: $accuracy).animation(.easeInOut), isActive: $active){
                         Text("开始模拟")
                     }
                     .onChange(of: active, perform: { value in
@@ -43,17 +45,22 @@ struct ContentView: View {
                             tmpSolver.set_value(alpha: Double(alpha)/10, beta: Double(beta)/10, m1: Double(m1), m2: Double(m2), w1: Double(w1)/10, w2: Double(w2)/10, l1: Double(l1), l2: Double(l2))
                         }
                     })
-                    #if targetEnvironment(macCatalyst)
                     Button("停止模拟", action: {
                         active = false
                     })
-                    #endif
                     VStack{
                         Slider(value: $zoom, in: 10.0...200.0)
                         Text("缩放级别: \(String(format: "%.1f", zoom))")
                             .font(.body)
                             .foregroundColor(.gray)
                     }
+                    VStack{
+                        Slider(value: $accuracy, in: 1...6)
+                        Text("精细度: \(String(format: "%.1f", accuracy))")
+                            .font(.body)
+                            .foregroundColor(.gray)
+                    }
+                    Text("fps: "+String(format: "%.1f", fps))
                 }
             }.navigationBarTitle("双摆模拟")
         }
--- 双摆/SwiftUIView.swift
+++ 双摆/SwiftUIView.swift
@@ -8,115 +8,13 @@
 import SwiftUI
 import SpriteKit
 
-class AnimateScene: SKScene, ObservableObject {
-    var lastUpdateTime: TimeInterval?
-    var line1 = SKShapeNode()
-    var line2 = SKShapeNode()
-    var swiftSolver: SwiftSolver
-    var cx: CGFloat = 0.0, cy: CGFloat = 0.0
-    var centerBox = SKShapeNode()
-    var firstBox = SKShapeNode()
-    var secondBox = SKShapeNode()
-    var zoom: CGFloat = 100.0
-    
-    func updateSolver(newSolver: SwiftSolver) {
-        swiftSolver.set_value(alpha: newSolver.alpha, beta: newSolver.beta, m1: newSolver.m1, m2: newSolver.m2, w1: newSolver.w1, w2: newSolver.w2, l1: newSolver.l1, l2: newSolver.l2)
-    }
-    
-    private func drawLine(line: inout SKShapeNode, from: CGPoint, to: CGPoint){
-        line.removeFromParent()
-        let path = CGMutablePath()
-        path.move(to: from)
-        path.addLine(to: to)
-        line = SKShapeNode(path: path)
-        self.addChild(line)
-    }
-    
-    private func updatePos() {
-        let (pos1, pos2) = swiftSolver.get_position()
-        
-        cx = self.size.width / 2
-        cy = self.size.height / 2
-        
-        centerBox.position = CGPoint(x:cx, y:cy)
-        firstBox.position = CGPoint(x: cx+pos1.x*zoom, y: cy-pos1.y*zoom)
-        secondBox.position = CGPoint(x: cx+pos2.x*zoom, y: cy-pos2.y*zoom)
-        
-        drawLine(line: &line1, from: centerBox.position, to: firstBox.position)
-        drawLine(line: &line2, from: firstBox.position, to: secondBox.position)
-    }
-    
-    override init() {
-        swiftSolver = SwiftSolver()
-        super.init()
-    }
-    
-    override init(size: CGSize) {
-        swiftSolver = SwiftSolver()
-        super.init(size: size)
-    }
-    
-    convenience init(swiftSolver: SwiftSolver) {
-        self.init()
-        self.swiftSolver = swiftSolver;
-        updatePos()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func sceneDidLoad() {
-        cx = self.size.width / 2
-        cy = self.size.height / 2
-        
-        centerBox = SKShapeNode(circleOfRadius: 6)
-        centerBox.fillColor = UIColor.red
-        centerBox.strokeColor = UIColor.red
-        centerBox.name = "center"
-        addChild(centerBox)
-        
-        firstBox = SKShapeNode(circleOfRadius: 6)
-        firstBox.fillColor = UIColor.green
-        firstBox.strokeColor = UIColor.green
-        firstBox.name = "first"
-        addChild(firstBox)
-        
-        secondBox = SKShapeNode(circleOfRadius: 6)
-        secondBox.fillColor = UIColor.yellow
-        secondBox.strokeColor = UIColor.yellow
-        secondBox.name = "second"
-        addChild(secondBox)
-        
-        // Update Position After Create
-        updatePos()
-    }
-    
-    override func didChangeSize(_ oldSize: CGSize) {
-        updatePos()
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        var timeInterval: Double = 0.0;
-        if lastUpdateTime != nil {
-            timeInterval = currentTime - lastUpdateTime!;
-        }
-        lastUpdateTime = currentTime;
-        timeInterval = min(timeInterval, 0.1)
-        
-        // Update with CPP Lib
-        swiftSolver.step(delta_t: timeInterval);
-        
-        // Update Pos
-        updatePos()
-    }
-}
-
 struct SwiftUIView: View{
     @ObservedObject var swiftSolver: SwiftSolver
     @Binding var active: Bool
     @Binding var zoom: Double
     @StateObject var spriteScene = AnimateScene(swiftSolver: SwiftSolver())
+    @Binding var fps: Double
+    @Binding var accuracy: Double
     
     func sizableScene(width: CGFloat, height: CGFloat) -> SKScene {
         let scene = AnimateScene(swiftSolver: swiftSolver)
@@ -126,9 +24,9 @@ struct SwiftUIView: View{
     }
     
     var body: some View {
-        #if targetEnvironment(macCatalyst)
+        #if targetEnvironment(macCatalyst) || os(iOS)
         GeometryReader(content: { geometry in
-            SpriteView(scene: spriteScene)
+            SpriteView(scene: spriteScene, preferredFramesPerSecond: 120)
                 .onAppear(perform: {
                     spriteScene.updateSolver(newSolver: swiftSolver)
                     spriteScene.zoom = CGFloat(zoom)
@@ -139,16 +37,23 @@ struct SwiftUIView: View{
                 })
                 .onChange(of: active, perform: { value in
                     if !active {
-                        spriteScene.isPaused = true
+                        spriteScene.manualPause(value: true)
                     }
                     else {
                         spriteScene.zoom = CGFloat(zoom)
                         spriteScene.swiftSolver = swiftSolver
-                        spriteScene.isPaused = false
+                        spriteScene.manualPause(value: false)
                     }
                 })
                 .onChange(of: zoom, perform: { value in
                     spriteScene.zoom = CGFloat(zoom)
+                })
+                .onChange(of: spriteScene.fps, perform: { value in
+                    fps = value
+                })
+                .onChange(of: accuracy, perform: { value in
+                    spriteScene.accuracy = pow(10, accuracy)
+                    print("Accuracy set to \(spriteScene.accuracy)")
                 })
         })
         .navigationBarHidden(true)
